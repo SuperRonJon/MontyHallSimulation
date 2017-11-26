5a1f93c05b1602f646964f95ef4788effa7abee6
pseute <pseut001@odu.edu>
2017-11-26 03:51:42 -0500

    fixed run function to swap to final choice

--- src/main/Simulation.java
+++ src/main/Simulation.java
@@ -4,32 +4,26 @@ public class Simulation {
 	private int iterations;
 	private int wins;
 	private int losses;
-	private int maxWins;
 	private double winPercentage;
 	
 	public Simulation(int its) {
 		iterations = its;
 		wins = 0;
 		losses = 0;
-		maxWins = 0;
 		winPercentage = 0.0;
 	}
 	
 	public void run() {
-		int winstreak = 0;
 		for(int i = 0; i < iterations; i++) {
 			Game game = new Game();
 			game.makeInitialChoice();
 			game.showEmptyDoor();
+			game.swapChoice();
 			if(game.checkWin()) {
-				wins++;
-				winstreak++;
-				if(winstreak > maxWins)
-				maxWins = wins;
+				wins++;			
 			}
 			else {
 				losses++;
-				winstreak = 0;
 			}
 		}
 	}
@@ -40,6 +34,5 @@ public class Simulation {
 		System.out.println("Wins: " + wins);
 		System.out.println("Losses: " + losses);
 		System.out.println("Win %: " + winPercentage);
-		System.out.println("Maximum wins in a row: " + maxWins);
 	}
 }
