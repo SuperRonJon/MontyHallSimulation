f53e364a3273fa38c5fe92ab6d3397755e1a8086
pseute <pseut001@odu.edu>
2017-11-26 13:05:31 -0500

    added ability to enter number of experiments

--- src/main/Driver.java
+++ src/main/Driver.java
@@ -1,9 +1,16 @@
 package main;
 
+import java.util.Scanner;
+
 public class Driver {
+	
+	static Scanner sc;
 
 	public static void main(String[] args) {
-		Simulation sim = new Simulation(10);
+		sc = new Scanner(System.in);
+		System.out.println("Enter number of experiments: ");
+		int iterations = sc.nextInt();
+		Simulation sim = new Simulation(iterations);
 		sim.run();
 		sim.showStats();
 
