486a6fa10f1ea252af74bf95201563d810a0f79c
pseute <pseut001@odu.edu>
2017-12-15 20:50:12 -0500

    fixed merge conflict

--- src/main/Simulation.java
+++ src/main/Simulation.java
@@ -1,42 +1,3 @@
-<<<<<<< HEAD
-package main;
-
-public class Simulation {
-	private int iterations;
-	private int wins;
-	private int losses;
-	private double winPercentage;
-	
-	public Simulation(int its) {
-		iterations = its;
-		wins = 0;
-		losses = 0;
-		winPercentage = 0.0;
-	}
-	
-	public void run() {
-		for(int i = 0; i < iterations; i++) {
-			Game game = new Game();
-			game.makeInitialChoice();
-			game.showEmptyDoor();
-			game.swapChoice();
-			if(game.checkWin()) {
-				wins++;			
-			}
-			else {
-				losses++;
-			}
-		}
-	}
-	
-	public void showStats() {
-		winPercentage = (double)wins/iterations;
-		
-		System.out.println("Wins: " + wins);
-		System.out.println("Losses: " + losses);
-		System.out.println("Win %: " + winPercentage);
-	}
-=======
 package main;
 
 public class Simulation {
@@ -74,5 +35,4 @@ public class Simulation {
 		System.out.println("Losses: " + losses);
 		System.out.println("Win %: " + winPercentage);
 	}
->>>>>>> 5be5e89379e5ab8c645203a41eff35aee6a937ba
 }
